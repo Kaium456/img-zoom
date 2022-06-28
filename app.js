c78d1a9bb222f577a66a81052a9813a3e3f18c68
Kaium456 <kaiumbd456@gmail.com>
2022-06-28 09:07:25 +0600

    all code done

---
+++ app.js
@@ -0,0 +1,11 @@
+let mainImg = document.getElementById("mainImg");
+
+function changeImg1() {
+  mainImg.src = "1.jpg";
+}
+function changeImg2() {
+  mainImg.src = "2.jpg";
+}
+function changeImg3() {
+  mainImg.src = "my.jpg";
+}
